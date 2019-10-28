2dc929d4df017ce4561346c0d93c35427ff964f7
Jaakounette <jeeqskill@gmail.com>
2019-10-28 11:07:55 +0100

    Added comments in CompteDouble.java

--- TP1/CompteDouble.java
+++ TP1/CompteDouble.java
@@ -1,4 +1,6 @@
 
+/* Boucle i initialisé à 1 allant jusqu'à 5 par incrémentation, j s'initialise à 11 et prend le double de i à chaque itération*/
+
 public class CompteDouble {
 	public static void main(String args[]) {
 		for(int i = 1, j=i+10;  i<5; i++, j=i*2) {
