c68e098636c6631e78213ed33d825e00bef35724
Jaakounette <jeeqskill@gmail.com>
2019-10-28 11:11:37 +0100

    Added comments on Promote.java

--- TP1/Promote.java
+++ TP1/Promote.java
@@ -11,13 +11,15 @@ public class Promote {
 		
 		System.out.print((double)(f * b) + " + " + (i / c) +  " - " + (d * s));
 		System.out.println(" = " + resultat);
-		
+		/* Affiche le calcul de la variable resultat avant d'aller à la ligne pour afficher le résultat */
 		byte b2 = 10;
 		byte b3 = (byte) (b2 * b);
 		
 		System.out.println("b = " + b);
 		System.out.println("b2 = " + b2);
 		System.out.println("b3 = " + b3);
+		
+		/* b3 stockes b2 * b, puis on affiche b, b2 et b3 */
 		
 	}
 }
